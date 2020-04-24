7b75f2070ef4c70cb667de528e40003a31472e91
Dylan Gleicher <dgleiche@me.com>
2020-04-24 00:30:04 -0400

    Added twitter access token/secret generated from site

--- app/jobs/twitter_monitor_job.rb
+++ app/jobs/twitter_monitor_job.rb
@@ -1,7 +1,11 @@
 class TwitterMonitorJob < ApplicationJob
   queue_as :default
 
+  # TODO: In a production project, move to a secret file
+  TWITTER_ACCESS_TOKEN = "1222968141036367872-GKPt0Zd9IOahMc8m74jrGCG0fIIs74"
+  TWITTER_TOKEN_SECRET = "mr6w53dprk4QfgKjdlsprQPlGBUOeKv0ySlbj6FHWmDSZ"
+
   def perform(*args)
-    # Do something later
+
   end
 end
