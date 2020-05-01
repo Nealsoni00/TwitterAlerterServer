69a48d35648a47b649a4564cf0a581f2421c75ca
Dylan Gleicher <dgleiche@me.com>
2020-05-01 12:10:54 -0400

    Added test suite for tweets

--- test/factories/tweet.rb
+++ test/factories/tweet.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :tweet do
-
+    text { Faker::Lorem.paragraph(sentence_count: 2) }
+    username { Faker::Name.name }
   end
 end
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,6 +1,17 @@
 require "test_helper"
 
 class TweetTest < ActiveSupport::TestCase
-  text { Faker::Lorem.paragraph(sentence_count: 2) }
-  username { Faker::Name.name }
+  test "create a valid tweet" do
+    tweet = build(:tweet)
+    assert tweet.save, "Could not save valid tweet"
+  end
+
+  test "should only get tweets within the last day" do
+    tweets = create_list(:tweet, rand(1..10))
+    assert_equal tweets.count, Tweet.last_day.count, "Unable to retrieve tweets in range"
+
+    future_date = Time.now + 25.hours
+    travel_to future_date
+    assert_equal 0, Tweet.last_day.count, "Retrieved tweets outside of a day"
+  end
 end
