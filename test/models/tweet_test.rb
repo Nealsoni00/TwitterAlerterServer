4cb025dea598a668e42f8cdfead12433a6cabd07
Dylan Gleicher <dgleiche@me.com>
2020-05-01 12:05:50 -0400

    Added tweet factory

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,5 +1,6 @@
-require 'test_helper'
+require "test_helper"
 
 class TweetTest < ActiveSupport::TestCase
-
+  text { Faker::Lorem.paragraph(sentence_count: 2) }
+  username { Faker::Name.name }
 end
